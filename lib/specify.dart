6406ae1e1545ac232df82249f2fa50631f6095f3
Yu Mitsuhori <ymbullseye@gmail.com>
2020-02-01 15:14:47 +0900

    widgetの実装

--- lib/specify.dart
+++ lib/specify.dart
@@ -1,7 +1,29 @@
 library specify;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Specify extends StatelessWidget {
+  final Widget android;
+  final Widget ios;
+
+  Specify({
+    @required this.android,
+    @required this.ios,
+  })  : assert(android != null),
+        assert(ios != null);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      return android;
+    } else if (Platform.isIOS) {
+      return ios;
+    } else {
+      throw PlatformException(
+          code: 'E01', message: 'This platform is not supported.');
+    }
+  }
 }
